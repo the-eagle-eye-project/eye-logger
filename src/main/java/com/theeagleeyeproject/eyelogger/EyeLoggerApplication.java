48c26ae5cc1cb3a192906c42c57f5b32043f89c6
jmart <jmartinez5120@gmail.com>
2023-08-02 22:50:37 -0400

    Intial commit.

---
+++ src/main/java/com/theeagleeyeproject/eyelogger/EyeLoggerApplication.java
@@ -0,0 +1,13 @@
+package com.theeagleeyeproject.eyelogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EyeLoggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EyeLoggerApplication.class, args);
+	}
+
+}
